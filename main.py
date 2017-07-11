fbd0b96cad62e757aa8fde923f31ccc520d5051d
holi0317 <holliswuhollis@gmail.com>
2017-07-11 15:57:41 +0800

    Add logging

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import os
 from time import sleep
 
 import json
+
+import logging
 from google.cloud import storage
 from google.oauth2 import service_account
 from picamera import PiCamera
 
 from streaming.main_loop import main_loop
+
+logger = logging.getLogger('Root')
+# Set up logging
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s / %(threadName)s / %(levelname)s] %(message)s',
+    '%H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 def storage_client_factory():
@@ -14,15 +26,24 @@ def storage_client_factory():
     if key is None:
         raise RuntimeError('GOOGLE_SERVICE_KEY environment variable is not set. Check README.md, service key section '
                            'for details')
-    credentials = service_account.Credentials.from_service_account_info(json.loads(key))
+    parse = json.loads(key)
+    logger.debug('Got google service key. It is valid JSON.')
+    credentials = service_account.Credentials.from_service_account_info(parse)
     return storage.Client(credentials=credentials)
 
 
 def main():
+    logger.info('Initializing')
+
     client = storage_client_factory()
+    logger.debug('Google cloud storage initialized')
+
     camera = PiCamera()
     camera.start_preview()
+    logger.info('Warming up camera')
     sleep(2)
+
+    logger.info('Initialization completed')
     main_loop(camera, client)
 
 if __name__ == '__main__':
--- streaming/main_loop.py
+++ streaming/main_loop.py
@@ -2,17 +2,28 @@ import threading
 from datetime import datetime
 from time import sleep
 
+import logging
+
 from streaming.take_picture import take_picture
 from streaming.upload import upload
+
+logger = logging.getLogger('MainLoop')
 
 
 def main_loop(camera, storage):
     while True:
         threading.Thread(target=tick, name="tick", args=[camera, storage])
-        sleep(1)
+        sleep(2)
 
 
 def tick(camera, storage):
+    logger.debug('Attempting to take picture')
     picture = take_picture(camera)
+    logger.debug('Picture successfully taken')
+
     time = datetime.utcnow()
+    logger.debug('Time now is %s', time)
+
+    logger.debug('Starting to upload')
     upload(picture, time, storage)
+    logger.debug('Upload completed')
