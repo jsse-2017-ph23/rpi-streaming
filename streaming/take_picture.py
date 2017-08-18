d088788dc551e7816b75436b6c98f3316295a045
holi0317 <holliswuhollis@gmail.com>
2017-08-18 10:07:51 +0800

    Rotate screen

--- streaming/take_picture.py
+++ streaming/take_picture.py
@@ -9,6 +9,7 @@ capture_lock = threading.Lock()
 def take_picture(camera):
     # Create an in-memory stream
     stream = BytesIO()
+    camera.rotation = 180
     camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with capture_lock:
         camera.capture(stream, 'jpeg', resize=(720, 480))
